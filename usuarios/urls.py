e60ea06ec7bd9ac6ee25ba92bd9a0775c1ab7a23
Thiago Dias Olimpio <thiago.olimpio93@gmail.com>
2024-03-01 13:23:54 -0300

    Configuração do app Usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from usuarios.views import login, cadastro
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('galeria.urls')),
-    path('', include('usuarios.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('login', login, name='login'),
+    path('cadastro', cadastro, name='cadastro'),
+]
